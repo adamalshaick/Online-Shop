dfecd4d41b9847b816eae6217e7760be72e8802d
unknown <adamalshaick@gmail.com>
2019-01-09 13:14:21 +0100

    profile actions tests

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -14,12 +14,12 @@ export const getProfileByHandle = handle => dispatch => {
   dispatch(setProfileLoading());
   axios
     .get(`/api/profile/handle/${handle}`)
-    .then(res =>
+    .then(res => {
       dispatch({
         type: GET_PROFILE,
         payload: res.data
-      })
-    )
+      });
+    })
     .catch(err =>
       dispatch({
         type: GET_PROFILE,
