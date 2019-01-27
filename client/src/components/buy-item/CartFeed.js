f50ef7b6dd4874b6d89c33b53c5102cd19ac8ca6
unknown <adamalshaick@gmail.com>
2019-01-27 02:45:27 +0100

    styled cart

--- client/src/components/buy-item/CartFeed.js
+++ client/src/components/buy-item/CartFeed.js
@@ -5,14 +5,38 @@ import CartItem from "./CartItem";
 class CartFeed extends Component {
   render() {
     const { cartItems } = this.props;
+    let cartValue = 0;
+    cartItems.forEach(item => {
+      cartValue = +item.price + cartValue;
+    });
     return (
-      <div>
-        <div className="row">
-          {cartItems.map(item => (
-            <CartItem key={item._id} item={item} />
-          ))}
+      <>
+        <div
+          style={{
+            width: "100%",
+            backgroundColor: "whitesmoke",
+            height: "60px",
+            border: "solid lightgrey 1px",
+            marginBottom: "20px",
+            marginTop: "50px",
+            padding: "14px"
+          }}
+        >
+          <span style={{ fontSize: "18px" }} className="d-inline align-middle">
+            <strong>Your cart</strong>
+          </span>
+          <span
+            className="align-middle"
+            style={{ fontSize: "18px", float: "right" }}
+          >
+            <strong>Amount: {cartValue} $</strong>
+          </span>
         </div>
-      </div>
+
+        {cartItems.map(item => (
+          <CartItem key={item._id} item={item} />
+        ))}
+      </>
     );
   }
 }
--- client/src/components/buy-item/CartItem.js
+++ client/src/components/buy-item/CartItem.js
@@ -1,38 +1,29 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getItem } from "../../actions/itemActions";
 import PropTypes from "prop-types";
 
 class CartItem extends Component {
   render() {
     const { item } = this.props;
-    // const { cartItem } = this.props.cartItem;
-
-    // console.log(cartItem._id);
     return (
-      <div className="col-4">
-        <div>
-          <div
-            style={{
-              boxShadow:
-                " 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
-            }}
-            className="text-center m-3"
-          >
-            <img
-              style={{ width: "100%", maxHeight: "500px" }}
-              className=" d-none d-md-block"
-              src={`../../uploads/post_image/${item.itemImage}`}
-              alt=""
-            />
-            <p className="lead">
-              <strong>{item.title}</strong>
-            </p>
-            <p style={{ fontSize: "2rem" }}>{item.price} $</p>
-          </div>
-        </div>
+      <div style={{ width: "100%" }}>
+        <img
+          style={{ height: "50px", width: "50px" }}
+          src={`../../uploads/post_image/${item.itemImage}`}
+          alt=""
+        />
+        {item.title}
+        <button
+          style={{ float: "right" }}
+          // onClick={this.onDeleteClick()}
+          className="btn btn-danger ml-3"
+        >
+          Remove
+        </button>
+        <span style={{ float: "right" }} className="mt-2">
+          {item.price} $
+        </span>
 
-        {/* <div className="col-12">{cartItem._id}</div> */}
+        <hr />
       </div>
     );
   }
