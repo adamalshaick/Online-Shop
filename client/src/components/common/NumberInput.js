e3d204d8aa4f859795a72de4da49cb3a1cff80d6
unknown <adamalshaick@gmail.com>
2019-02-02 20:59:10 +0100

    created number input component

---
+++ client/src/components/common/NumberInput.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+const NumberInput = ({ name, value, error, type, onChange }) => {
+  return (
+    <input
+      name={name}
+      value={value}
+      onChange={onChange}
+      style={{ width: "30px", fontSize: "1.2rem" }}
+      className={classnames("form-control form-control-sm ml-3", {
+        "is-invalid": error
+      })}
+    />
+  );
+};
+
+NumberInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  error: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+NumberInput.defaultProps = {
+  type: "text"
+};
+
+export default NumberInput;
