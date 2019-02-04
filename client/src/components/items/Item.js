da94bef81f64a7bdd2a7a5733b8dfc5caac2d60e
unknown <adamalshaick@gmail.com>
2019-02-04 04:02:37 +0100

    styling

--- client/src/components/items/Item.js
+++ client/src/components/items/Item.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { deleteItem } from "../../actions/itemActions";
 import { addItemToCart } from "../../actions/cartActions";
+import { Link } from "react-router-dom";
 import ReactTimeout from "react-timeout";
 import Alert from "../common/Alert";
 import styled from "styled-components";
@@ -15,6 +16,23 @@ const ItemCard = styled.div`
 const Image = styled.img`
   width: 100%;
   max-height: 400px;
+  ${ItemCard}:hover & {
+    opacity: 0.5;
+    cursor: pointer;
+    transition: opacity 0.25s;
+  }
+`;
+
+const Buttons = styled.div`
+  display: none;
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  ${ItemCard}:hover & {
+    display: block;
+  }
 `;
 
 class Item extends Component {
@@ -80,28 +98,27 @@ class Item extends Component {
                     <i className="fas fa-times" />
                   </button>
                 ) : this.state.showAlert ? (
-                  <>
-                    <div
-                      style={{
-                        width: "200px",
-                        height: "38px",
-                        visibility: "hidden"
-                      }}
-                    />
-                    <Alert
-                      className="entry"
-                      style={{}}
-                      showAlert={this.state.showAlert}
-                      text={item.title + " added to cart"}
-                    />
-                  </>
+                  <Alert
+                    className="entry"
+                    showAlert={this.state.showAlert}
+                    text={item.title + " added to cart"}
+                  />
                 ) : (
-                  <button
-                    onClick={this.onAddClick.bind(this, item)}
-                    className="btn btn-dark"
-                  >
-                    Add to your cart
-                  </button>
+                  <Buttons>
+                    <button
+                      onClick={this.onAddClick.bind(this, item)}
+                      className="btn btn-dark mb-2"
+                    >
+                      Add to your cart
+                    </button>
+                    <Link
+                      to="/jd"
+                      onClick={this.onAddClick.bind(this, item)}
+                      className="btn btn-dark mt-2"
+                    >
+                      Seller's profile
+                    </Link>
+                  </Buttons>
                 )}
               </span>
             ) : null}
