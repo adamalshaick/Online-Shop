0ec2471c7e95bf94dbb1a7e76d5aff4841f4c380
unknown <adamalshaick@gmail.com>
2019-03-03 00:45:39 +0100

    splited components

---
+++ client/src/components/reviews/AddReview.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addReview } from "../../actions/reviewActions";
+import ReviewForm from "../reviews/ReviewForm";
+
+class AddReview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showReviewInput: false,
+      text: "",
+      rate: "",
+      errors: {}
+    };
+  }
+  componentWillReceiveProps(newProps) {
+    if (newProps.errors) {
+      this.setState({ errors: newProps.errors });
+    }
+  }
+
+  onClickRev = () => {
+    this.state.showReviewInput
+      ? this.setState({ showReviewInput: false })
+      : this.setState({ showReviewInput: true });
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = (id, name) => e => {
+    e.preventDefault();
+    const reviewData = {
+      text: this.state.text,
+      rate: this.state.rate,
+      id: id,
+      name: name
+    };
+    this.props.addReview(reviewData);
+  };
+  render() {
+    const { errors, auth, profile } = this.props;
+    return (
+      <>
+        <button
+          style={{ border: "lightgray solid 1px", width: "100%" }}
+          className="btn btn-light btn-lg mt-2 mb-3"
+          onClick={this.onClickRev}
+        >
+          Write a review
+        </button>
+        <div>
+          {this.state.showReviewInput ? (
+            <ReviewForm
+              onClickRev={this.onClickRev}
+              text={this.state.text}
+              rate={this.state.rate}
+              errors={errors}
+              onChange={this.onChange}
+              onSubmit={this.onSubmit(
+                profile.user._id,
+                auth.user.name,
+                auth.user.avatar
+              )}
+            />
+          ) : null}
+        </div>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { addReview }
+)(AddReview);
