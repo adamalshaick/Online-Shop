3defe467dc70b89b29ebc3e71316aadf47ba8dc3
unknown <adamalshaick@gmail.com>
2019-03-04 21:15:33 +0100

    profile components actions test

--- client/src/__tests__/components/create-profile/CreateProfile.test.js
+++ client/src/__tests__/components/create-profile/CreateProfile.test.js
@@ -1,25 +1,17 @@
 import React from "react";
-import { EditProfile } from "../../../components/edit-profile/EditProfile";
+import { CreateProfile } from "../../../components/create-profile/CreateProfile";
 import { shallow } from "enzyme";
 import toJson from "enzyme-to-json";
 
 const mockCreateProfilefn = jest.fn();
-const mockgetCurrentProfilefn = jest.fn();
-
 const Props = {
-  profile: {},
   errors: {}
 };
 let wrapper;
 
 wrapper = shallow(
-  <EditProfile
-    {...Props}
-    createProfile={mockCreateProfilefn}
-    getCurrentProfile={mockgetCurrentProfilefn}
-  />
+  <CreateProfile {...Props} createProfile={mockCreateProfilefn} />
 );
-
 describe("render component", () => {
   it("renders component", () => {
     expect(toJson(wrapper)).toMatchSnapshot();
@@ -30,26 +22,23 @@ it("displays form", () => {
   expect(wrapper.find("form").length).toEqual(1);
 });
 
-it("should call the mock edit profile function", () => {
+it("should call the mock create profile function", () => {
   wrapper.find("form").simulate("submit", { preventDefault() {} });
   expect(mockCreateProfilefn.mock.calls.length).toBe(1);
 });
 
-describe("edit profile action", () => {
-  const profileData = {
-    handle: "handle value",
-    location: "location value",
-    bio: "bio value"
-  };
+describe("create profile action", () => {
+  const profileData = new FormData();
+  profileData.append("handle", "handle value");
+  profileData.append("location", "location value");
+  profileData.append("bio", "bio value");
 
   wrapper
     .find("#handle")
     .simulate("change", { target: { name: "handle", value: "handle value" } });
-
   wrapper.find("#location").simulate("change", {
     target: { name: "location", value: "location value" }
   });
-
   wrapper
     .find("#bio")
     .simulate("change", { target: { name: "bio", value: "bio value" } });
