215b1349cefb730f7d0e849a8b4037e61ffc8e17
unknown <adamalshaick@gmail.com>
2019-04-06 22:39:37 +0200

    bugfix

--- client/src/components/items/Item.js
+++ client/src/components/items/Item.js
@@ -7,59 +7,57 @@ import { Link } from "react-router-dom";
 import { ItemCard, Image, Buttons } from "../common/styles/StyledItem";
 import { SecondaryHeader } from "../common/styles/Header";
 
+const onAdd = (id, addItemToCart) => e => {
+  e.preventDefault();
+  addItemToCart(id);
+};
+
+const onDelete = (id, deleteItem) => e => {
+  e.preventDefault();
+  deleteItem(id);
+};
+
 export const Item = ({ item, currentUser, addItemToCart, deleteItem }) => {
-  const onAdd = id => e => {
-    e.preventDefault();
-    addItemToCart(id);
-  };
-
-  const onDelete = id => e => {
-    e.preventDefault();
-    deleteItem(id);
-  };
-
   return (
     <div className="col-md-6 col-lg-4 p-0 entry-2x">
-      <div>
-        <ItemCard className="text-center m-3">
-          <Image src={item.itemImage} alt="" />
-          <p>{item.name}</p>
-          <SecondaryHeader>{item.price} $</SecondaryHeader>
-          <span>
-            {item.user === currentUser._id ? (
-              <button
-                id="deleteItem"
-                onClick={onDelete(item._id)}
-                className="btn btn-danger mr-1"
-              >
-                <i className="fas fa-times" />
-              </button>
-            ) : (
-              <Buttons>
-                {currentUser.cart.some(
-                  cartItem => cartItem._id === item._id
-                ) ? null : (
-                  <>
-                    <button
-                      id="addToCart"
-                      onClick={onAdd(item._id)}
-                      className="btn btn-dark mb-2"
-                    >
-                      Add to your cart
-                    </button>
-                    <Link
-                      to={`/profile/${item.user}`}
-                      className="btn btn-dark mt-2"
-                    >
-                      Seller's profile
-                    </Link>
-                  </>
-                )}
-              </Buttons>
-            )}
-          </span>
-        </ItemCard>
-      </div>
+      <ItemCard className="text-center m-3">
+        <Image src={item.itemImage} alt="" />
+        <p>{item.name}</p>
+        <SecondaryHeader>{item.price} $</SecondaryHeader>
+        <span>
+          {item.user === currentUser._id ? (
+            <button
+              id="deleteItem"
+              onClick={onDelete(item._id, deleteItem)}
+              className="btn btn-danger mr-1"
+            >
+              <i className="fas fa-times" />
+            </button>
+          ) : (
+            <Buttons>
+              {currentUser.cart.some(
+                cartItem => cartItem._id === item._id
+              ) ? null : (
+                <>
+                  <button
+                    id="addToCart"
+                    onClick={onAdd(item._id, addItemToCart)}
+                    className="btn btn-dark mb-2"
+                  >
+                    Add to your cart
+                  </button>
+                  <Link
+                    to={`/profile/${item.user}`}
+                    className="btn btn-dark mt-2"
+                  >
+                    Seller's profile
+                  </Link>
+                </>
+              )}
+            </Buttons>
+          )}
+        </span>
+      </ItemCard>
     </div>
   );
 };
